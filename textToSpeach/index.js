54be6ca50c3e3dd9bf134333518d661fdd57be7c
hassanamin994 <hassanmamin994@gmail.com>
2020-06-29 03:36:46 +0200

    add audio speed param for tts

--- textToSpeach/index.js
+++ textToSpeach/index.js
@@ -1,21 +1,39 @@
-const superagent = require('superagent');
-const fs = require('fs')
+const superagent = require("superagent");
+const fs = require("fs");
 
-module.exports = TEXT_TO_SPEECH_SERVICE_API_ROOT => {
-    function convertTextToSpeech({ speakersProfile, speakerNumber, langCode, text, outputFormat }, targetPath) {
-        return new Promise((resolve, reject) => {
-            superagent.post(TEXT_TO_SPEECH_SERVICE_API_ROOT, { speakersProfile, speakerNumber, langCode, text, outputFormat })
-                .then((res) => {
-                    fs.writeFile(targetPath, res.body, (err) => {
-                        if (err) return reject(err);
-                        return resolve(targetPath);
-                    })
-                })
-                .catch(reject)
+module.exports = (TEXT_TO_SPEECH_SERVICE_API_ROOT) => {
+  function convertTextToSpeech(
+    {
+      speakersProfile,
+      speakerNumber,
+      langCode,
+      text,
+      audioSpeed,
+      outputFormat,
+    },
+    targetPath
+  ) {
+    return new Promise((resolve, reject) => {
+      superagent
+        .post(TEXT_TO_SPEECH_SERVICE_API_ROOT, {
+          speakersProfile,
+          speakerNumber,
+          langCode,
+          text,
+          audioSpeed,
+          outputFormat,
         })
-    }
-    
-    return  {
-        convertTextToSpeech
-    }
-}
+        .then((res) => {
+          fs.writeFile(targetPath, res.body, (err) => {
+            if (err) return reject(err);
+            return resolve(targetPath);
+          });
+        })
+        .catch(reject);
+    });
+  }
+
+  return {
+    convertTextToSpeech,
+  };
+};
